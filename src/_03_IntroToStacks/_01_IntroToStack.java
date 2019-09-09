d1446db1830cc82bbced6b6da609cf570df0b0e8
League Student <league@iMac-7.attlocal.net>
2019-09-08 17:29:00 -0700

    IntroToStacks Finished
    IntroToStacks Finished, TextUndoRedo (IP), Brackets (IP)

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -3,17 +3,31 @@ package _03_IntroToStacks;
 import java.util.Random;
 import java.util.Stack;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		int zeronumber;
+		int onenumber;
+		String twonumbers = JOptionPane.showInputDialog("Enter two numbers inbetween 0 and 100 with a space inbetween");
+		String[] name = twonumbers.split(" ");
+		zeronumber = Integer.parseInt(name[0]);
+		onenumber = Integer.parseInt(name[1]);
+		Stack <Double> number = new Stack <Double>();
+		for (Double i = 0.0; i <101; i++) {
+			number.push(i);
+			if (i > zeronumber && i < onenumber) {
+				JOptionPane.showMessageDialog(null, number.pop());
+			}
+		}
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 		
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+	
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
 		
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,9 +1,18 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
+import javax.swing.plaf.basic.BasicSplitPaneUI.KeyboardDownRightHandler;
+
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
+	 *
 	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
 	 * 
 	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
@@ -13,6 +22,42 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+    JLabel label = new JLabel();
+	public static void main(String[] args) {
+    _02_TextUndoRedo textredo = new _02_TextUndoRedo();
+    textredo.setup();
+
+
+	}
+	void setup() {
+	    frame.add(panel);
+	    panel.add(label);
+	    frame.addKeyListener(this);
+	    frame.setVisible(true);
+		
+	}
+	
+	
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char character = e.getKeyChar();
+		label.setName(character + "");
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
