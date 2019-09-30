595937490e3e38532a0be973e8a81c48dd1a17a2
Sofia Velarde <sofiavelarde2004@gmail.com>
2019-09-29 17:25:33 -0700

    Hangman (IP)
    Hangman (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,15 +1,19 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class HangMan{
 	public static void main(String[] args) {
 		HangMan hangman = new HangMan();
+		hangman.question();
 		hangman.setup();
-		hangman.firstPhase();
+		
 		
 	}
 	
@@ -18,24 +22,40 @@ public class HangMan{
 	JPanel panel;
 	JTextField field;
 	JLabel label;
+	
+	void question() {
+		String answer = JOptionPane.showInputDialog("Guess a number up to 266");
+		Stack <String> words = new Stack <String>();
+		int converted = Integer.parseInt(answer);
+		for (int i = 0; i < converted; i++) {
+			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+			System.out.println(words.pop());
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
 	void setup() {
 		frame = new JFrame();
 		panel = new JPanel();
 		field = new JTextField();
 		label = new JLabel();
 		frame.setSize(500,500);
+		frame.setVisible(true);
 		frame.add(panel);
 		panel.add(field);
+		panel.add(label);
+		
 	   
 	}
 	
 	void firstPhase() {
-		label.setText("Guess a number up to 266");
-		String recieved = field.getText();
-		int input = Integer.parseInt(recieved);
-		//for (int i = 0; i < input; i++) {
-			
-		//}
+		
+	
 	}
 	
 }
